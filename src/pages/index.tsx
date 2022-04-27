c6480f0f0131eb9d74f604bfe39190569f2ddd42
Jonathan Thomsen <jthomsen.itj@gmail.com>
2022-04-27 15:42:58 -0300

    Exemplo de Static Site Generation(SSG) para carregamento da página de forma estática

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-interface HomePropos {
+interface HomeProps {
   product: {
     priceId: string;
     amount: number;
   }
 }
 
-export default function Home({ product }: HomePropos) {
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -34,7 +34,7 @@ export default function Home({ product }: HomePropos) {
   )  
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KtEbKA5okxdJU6ltwsw39Lv')
 
   const product = {
@@ -49,6 +49,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return { 
     props: {
       product
-    }
+    }, 
+    revalidate: 60 * 60 * 24, //24 horas
   }
 }
